d36a5e2a760aa4c156dad5959a557cee6f6ae94d
alexvillr <73649305+alexvillr@users.noreply.github.com>
2023-12-02 00:40:35 +1000

    Added day 1 solutions

---
+++ day-1/puzzle.py
@@ -0,0 +1,34 @@
+numbers = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+]
+part1 = 0
+part2 = 0
+with open('small-input') as file:
+    for row in file:
+        part1set = []
+        part2set = []
+        index = 0
+        while index < len(row):
+            if row[index].isdigit():
+                part1set.append(row[index])
+                part2set.append(row[index])
+            for num in numbers:
+                if (index < (len(row) - len(num))) and (
+                    row[index : index + len(num)] == num
+                ):
+                    part2set.append(numbers.index(num) + 1)
+            index += 1
+        if len(part1set) != 0:
+            part1 += int(f'{part1set[0]}{part1set[-1]}')
+        if len(part2set) != 0:
+            part2 += int(f'{part2set[0]}{part2set[-1]}')
+    print(part1)
+    print(part2)
